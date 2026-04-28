b0362105deeb4c01b2c56c6b37215a4a39002368
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-04-28 10:47:10 +0100

    Finished top movies section

--- src/components/movies/MovieCard.jsx
+++ src/components/movies/MovieCard.jsx
@@ -1,10 +1,21 @@
 export default function MovieCard({ movie }) {
     return (
         <div className="col-span-12 md:col-span-6 lg:col-span-4">
-            <div className="w-[80%] border border-white rounded-md overflow-auto">
-                <img src={movie.image} alt={movie.title} />
+            <div className="relative w-[80%] border border-white rounded-md overflow-auto">
+                <span className="absolute top-2 right-2 bg-[#ddd] text-black px-2 rounded-md border">
+                    {movie.rating}
+                </span>
+                <img
+                    src={movie.image}
+                    alt={movie.title}
+                    className="w-full h-[225px]"
+                />
                 <div id="movie-info" className="p-4">
-                    <h3>{movie.title}</h3>
+                    <h3 className="text-xl font-bold">{movie.title}</h3>
+                    <span className="text-sm">{movie.gender}</span>
+                    <button className="block bg-transparent text-white text-sm px-4 py-2 mt-2 mx-auto border border-white rounded-md cursor-pointer">
+                        View Details
+                    </button>
                 </div>
             </div>
         </div>
--- src/components/movies/TopMovies.jsx
+++ src/components/movies/TopMovies.jsx
@@ -5,7 +5,7 @@ export default function TopMovies() {
     const topMoviesList = topMovies.map((el) => <MovieCard movie={el} />);
     return (
         <div>
-            <h2 className="text-xl font-bold mb-4">List of Top Movies</h2>
+            <h2 className="text-2xl font-bold mb-4">List of Top Movies</h2>
             <div className="grid grid-cols-12 gap-5">{topMoviesList}</div>
         </div>
     );
