981d84eb862ee4fef3a68314629b59adfb031372
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-04-30 21:28:44 +0100

    Finished get top 3 movies task and updated hero section movie details design

--- src/components/hero/HeroSection.jsx
+++ src/components/hero/HeroSection.jsx
@@ -10,13 +10,22 @@ export default function HeroSection({ movie }) {
         <section className="container relative text-white h-[calc(100%-72px)] flex flex-col justify-center gap-3">
             <h1 className="text-5xl font-bold">{movie.title}</h1>
             <p className="w-[50%]">{movie.description}</p>
-            <div className="text-sm">
-                <span>{movie.category}</span> • <span>{movie.releaseYear}</span>{" "}
-                •{" "}
-                <span className="flex items-center">
-                    <RiStarFill className="w-[11px]" />{" "}
-                    <span>{`${movie.rating}/5`}</span>
-                </span>
+            <div className="flex flex-col gap-1 text-sm">
+                <div>
+                    <span>Category: </span>
+                    <span>{movie.category}</span>
+                </div>
+                <div>
+                    <span>Release Year: </span>
+                    <span>{movie.releaseYear}</span>
+                </div>
+                <div className="flex items-center gap-1">
+                    <span>Rating: </span>
+                    <span className="flex items-center gap-1">
+                        <RiStarFill className="w-[16px] text-[#FFC107]" />
+                        <span>{`${movie.rating}/5`}</span>
+                    </span>
+                </div>
             </div>
             <button
                 className="flex items-center bg-transparent text-white text-sm w-fit border border-white hover:bg-white hover:text-black px-4 py-2 rounded-3xl cursor-pointer"
--- src/components/movies/TopMovies.jsx
+++ src/components/movies/TopMovies.jsx
@@ -1,13 +1,21 @@
 import MovieCard from "./MovieCard";
 
 export default function TopMovies({ movies }) {
-    const topMoviesList = movies.map((el) => (
+    const topRating = Math.max(...movies.map((movie) => movie.rating));
+
+    const topMovies = movies
+        .filter((movie) => movie.rating == topRating)
+        .slice(0, 3);
+
+    const topMoviesList = topMovies.map((el) => (
         <MovieCard key={el.id} movie={el} />
     ));
     return (
         <div>
             <h2 className="text-2xl font-bold mb-6">List of Top Movies</h2>
-            <div className="grid grid-cols-12 gap-5">{topMoviesList}</div>
+            <div className="grid max-md:grid-cols-12 min-lg:grid-cols-9 gap-5 max-lg:max-w-[400px] max-lg:mx-auto">
+                {topMoviesList}
+            </div>
         </div>
     );
 }
